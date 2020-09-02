eb535257df54f1f9e77d19a887aa913ea9e6f924
peterjewicz <peterjewicz@totalwebconnections.com>
2020-09-02 15:05:58 -0500

    basic styles for npc inv

--- src/wizard_lord/views/sub/npc_inventory.cljs
+++ src/wizard_lord/views/sub/npc_inventory.cljs
@@ -14,8 +14,12 @@
          [:div.Page__inner
           [:h1 {:on-click #(handle-state-change {:type "toggle-sub-view" :value {:sub-view "npc-inventory" :value false}})} "close"]
           [:div.NPCInventory__tabContent
-            (for [item (@currentTab inv)]
-              [:p {:key (:name item)} (str (:name item) " - " (:cost item) "GP")])]
+            [:div.NPCInventory__tabContent__left.tab
+             (for [item (@currentTab inv)]
+               [:p {:key (:name item)} (str (:name item) " - " (:cost item) "GP")])]
+            [:div.NPCInventory__tabContent__right.tab
+              [:h2 "Name of item"]
+              [:p "Fun description of the item"]]]
           [:div.NPCInventory__tabsWrapper
             (for [tab tabs]
               [:div.NPCInventory__tabsWrapper__tab {:key tab}
